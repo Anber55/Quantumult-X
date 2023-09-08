8f9a8b6b2060bb477e2b859131fd3354c494e9ca
Anber55 <117334130+Anber55@users.noreply.github.com>
2023-09-08 09:03:30 +0800

    Create SouTuShenQi.js

---
+++ SouTuShenQi.js
@@ -0,0 +1,3 @@
+# 搜图神器
+# hostname = wallpaper.soutushenqi.com
+^http:\/\/wallpaper\.soutushenqi\.com\/api\/.+\/account\/token url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/soutu.js
